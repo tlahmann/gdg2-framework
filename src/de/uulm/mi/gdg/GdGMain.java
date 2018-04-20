a94879387ac2249b271519f826dd44a0ca12edb4
Tobias Lahmann <tobias.lahmann@uni-ulm.de>
2018-04-21 00:31:36 +0200

    And suddenly it's working :dizzy_face:

--- src/de/uulm/mi/gdg/GdGMain.java
+++ src/de/uulm/mi/gdg/GdGMain.java
@@ -12,117 +12,75 @@ import java.util.ArrayList;
 
 public class GdGMain extends PApplet {
     private static GUI gui;
-    private JSONObject settings;
-    private String settingsPath;
 
-    private volatile ArrayList<CustomAnimation> anis;
+    private ArrayList<CustomAnimation> anis;
     private CustomAnimation ani;
 
     //    BufferedReader reader;
-    public AniExporter ae;
+    private AniExporter ae;
+//    private Thread aet;
 
-    float background = 255;
+    private float background = 255;
 
-    @Override
+    private boolean exporting = false;
+
     public void settings() {
-        this.settingsPath = "data" + File.separator + "settings.json";
-        File var5 = new File(this.settingsPath);
-        if (var5.isFile()) {
-            this.settings = this.loadJSONObject(this.settingsPath);
-        } else {
-            this.settings = new JSONObject();
-        }
-
-        Object width = this.settings.get("width");
-        Object height = this.settings.get("height");
-
-        if (width instanceof Integer && height instanceof Integer) {
-            setSize((int) width, (int) height);
-        }
+        setSize(1240, 720);
     }
 
     @Override
     public void setup() {
         // Produce the video as fast as possible
-        frameRate(100);
+        frameRate(60);
 
         // Read a sound file and output a txt file
         // with the FFT analysis.
         // It uses Minim, because the standard
         // Sound library did not work in my system.
 
-        Player p = new Player(this, "." + File.separator + "data" + File.separator + this.settings.get("song"));
+        Player p = new Player(this, "./data/Syntask - The Best of LMMS Vol.1 - 01 Galaksy.mp3");
         gui = new GUI(this, p);
-        AniImporter.audioToTextFile(this, this.settings.get("song").toString(), p.getSample(), "|");
-
-        ae = new AniExporter(this, this.settings.get("song").toString(), "GdG-export.mp4");
-
-        // Now open the text file we just created for reading
-//        reader = createReader(audioFilePath + ".txt");
-
+        ae = new AniExporter(this, "Syntask - The Best of LMMS Vol.1 - 01 Galaksy.mp3", "GdG-export.mp4");
+//        aet = new Thread(ae);
+//        aet.start();
 
         Ani.init(this);
 
         startAnimation();
     }
 
+    float movieFPS = 5;
+    float frameDuration = (1 / movieFPS) * 1000;
+    float lastFrame = System.currentTimeMillis();
+
     @Override
     public void draw() {
 //        if (ae.isExporting()) return;
+
         background(background);
         stroke(140, 69, 24);
         HUD_text(new String[]{"Gdg2", frameRate + "", frameCount + ""});
-        noFill();
+        fill(123, 23, 200);
         rect(this.width * 0.12f, this.height * 0.33f, 300, 400);
         ellipse(900, 300, 100, 200);
         triangle(300, 450, 650, 20, 1000, 450);
-        if (gui.getAudioPlayer().getSong().position() < 10000) {
-            ae.pushFrame(gui.getAudioPlayer().getSong().position() / 1000f, this.g);
-        } else if (!ae.isExporting()) {
-            ae.startExporting();
-//            this.stop();
-        }
-//        pushMatrix();
-        /*String line;
-//        System.out.println(this.g);
-        if (gui.getAudioPlayer().isPlaying()) {
-//            System.out.println("exporting");
-            try {
-                line = reader.readLine();
-            } catch (IOException e) {
-                e.printStackTrace();
-                line = null;
-            }
-            // End when we have exported enough frames
-            // to match the sound duration.
-            if (line == null) {
-                // Done reading the file.
-                // Close the video file.
-                videoExport.endMovie();
-                exit();
-            } else {
-//                System.out.println(line);
-                String[] p = split(line, SEP);
-                // The first column indicates
-                // the sound time in seconds.
-                float soundTime = Float.parseFloat(p[0]);
-                System.out.println(gui.getAudioPlayer().getSong().position());
-//                System.out.println(videoExport.getCurrentTime() + ":::" + soundTime);
-                while (videoExport.getCurrentTime() < soundTime + frameDuration * 0.5f) { //gui.getAudioPlayer().getSong().position()) {
-                    update(soundTime);
-                    display();
-                    videoExport.saveFrame();
-                }
-            }
+        update(gui.getAudioPlayer().getSong().position());
+        display();
+        ae.setSoundTime(gui.getAudioPlayer().getSong().position());
+//        System.out.println(gui.getAudioPlayer().getSong().position());
+        /*if (System.currentTimeMillis() > lastFrame + frameDuration) {
+            ae.soundTime = gui.getAudioPlayer().getSong().position();
+//            ae.startExporting();
+            lastFrame = System.currentTimeMillis();
         }*/
     }
 
     /**
      * HUD text upper left corner
      *
-     * @param texts
+     * @param texts array of texts to display
      */
-    void HUD_text(String[] texts) {
+    private void HUD_text(String[] texts) {
         textSize(16);
         fill(57, 255, 20, 204);
         for (int i = 0; i < texts.length; i++) {
@@ -133,7 +91,7 @@ public class GdGMain extends PApplet {
     /**
      * Initializes the animations into the anis-list to get a whole new start even if the song restarts.
      */
-    public void startAnimation() {
+    private void startAnimation() {
         anis = AniImporter.importAnimation(this, "./data/timing/timing.json", "background");
     }
 
@@ -149,7 +107,7 @@ public class GdGMain extends PApplet {
         if (anis.size() == 0) {
             return;
         }
-        if (val < anis.get(0).getStart()) {
+        if (val / 1000 < anis.get(0).getStart()) {
             return;
         }
 
@@ -167,8 +125,15 @@ public class GdGMain extends PApplet {
         ani = null;
     }
 
-    private static JSONArray toJSONArray(String var0) {
-        return new JSONArray(new StringList(var0.split(" ")));
+    public void keyPressed() {
+        if (key == 'e' || key == 'E') {
+            if (ae.isRunning()) {
+                ae.endExporting();
+            } else {
+                ae.startExporting();
+                gui.playPause(0);
+            }
+        }
     }
 
     public static void main(String[] args) {
