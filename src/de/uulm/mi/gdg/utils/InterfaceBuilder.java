d8450de23e38cd6d3010127f8af65321a7177402
Tobias Lahmann <tobias.lahmann@uni-ulm.de>
2018-04-23 01:07:03 +0200

    Change active colors

--- src/de/uulm/mi/gdg/utils/InterfaceBuilder.java
+++ src/de/uulm/mi/gdg/utils/InterfaceBuilder.java
@@ -38,8 +38,9 @@ class InterfaceBuilder {
                 .setSize(getWidth(text), 36)
                 .setFont(font)
                 .setColor(determineColor(color))
-                .setRange(0, 100) // values can range from big to small as well
-                .setValue(50);
+                .setRange(0, 100)
+                .setValue(50)
+                .plugTo(GdGMain.canvas, methodName);
     }
 
     void createToggle(ControlP5 cp5, String text, String color, int xPosition, int yPosition, String methodName) {
@@ -120,82 +121,82 @@ class InterfaceBuilder {
     private interface Color {
         CColor RED = new CColor(GdGMain.canvas.color(211, 47, 47),
                 GdGMain.canvas.color(244, 67, 54),
-                GdGMain.canvas.color(198, 40, 40),
+                GdGMain.canvas.color(198,40,40),
                 GdGMain.canvas.color(255, 255, 255),
                 GdGMain.canvas.color(255, 255, 255));
         CColor PINK = new CColor(GdGMain.canvas.color(194, 24, 91),
                 GdGMain.canvas.color(233, 30, 99),
-                GdGMain.canvas.color(233, 30, 99),
+                GdGMain.canvas.color(173,20,87),
                 GdGMain.canvas.color(255, 255, 255),
                 GdGMain.canvas.color(255, 255, 255));
         CColor PURPLE = new CColor(GdGMain.canvas.color(123, 31, 162),
                 GdGMain.canvas.color(156, 39, 176),
-                GdGMain.canvas.color(156, 39, 176),
+                GdGMain.canvas.color(106,27,154),
                 GdGMain.canvas.color(255, 255, 255),
                 GdGMain.canvas.color(255, 255, 255));
         CColor DEEP_PURPLE = new CColor(GdGMain.canvas.color(81, 45, 168),
                 GdGMain.canvas.color(103, 58, 183),
-                GdGMain.canvas.color(103, 58, 183),
+                GdGMain.canvas.color(69,39,160),
                 GdGMain.canvas.color(255, 255, 255),
                 GdGMain.canvas.color(255, 255, 255));
         CColor INDIGO = new CColor(GdGMain.canvas.color(48, 63, 159),
                 GdGMain.canvas.color(63, 81, 181),
-                GdGMain.canvas.color(63, 81, 181),
+                GdGMain.canvas.color(40,53,147),
                 GdGMain.canvas.color(255, 255, 255),
                 GdGMain.canvas.color(255, 255, 255));
         CColor BLUE = new CColor(GdGMain.canvas.color(25, 118, 210),
                 GdGMain.canvas.color(33, 150, 243),
-                GdGMain.canvas.color(33, 150, 243),
+                GdGMain.canvas.color(21,101,192),
                 GdGMain.canvas.color(0, 0, 0),
                 GdGMain.canvas.color(255, 255, 255));
         CColor LIGHT_BLUE = new CColor(GdGMain.canvas.color(2, 136, 209),
                 GdGMain.canvas.color(3, 169, 244),
-                GdGMain.canvas.color(3, 169, 244),
+                GdGMain.canvas.color(2,119,189),
                 GdGMain.canvas.color(0, 0, 0),
                 GdGMain.canvas.color(255, 255, 255));
         CColor CYAN = new CColor(GdGMain.canvas.color(0, 151, 167),
                 GdGMain.canvas.color(0, 188, 212),
-                GdGMain.canvas.color(0, 188, 212),
+                GdGMain.canvas.color(0,131,143),
                 GdGMain.canvas.color(0, 0, 0),
                 GdGMain.canvas.color(255, 255, 255));
         CColor TEAL = new CColor(GdGMain.canvas.color(0, 121, 107),
                 GdGMain.canvas.color(0, 150, 136),
-                GdGMain.canvas.color(0, 150, 136),
+                GdGMain.canvas.color(0,105,92),
                 GdGMain.canvas.color(255, 255, 255),
                 GdGMain.canvas.color(255, 255, 255));
         CColor GREEN = new CColor(GdGMain.canvas.color(56, 142, 60),
                 GdGMain.canvas.color(76, 175, 80),
-                GdGMain.canvas.color(76, 175, 80),
+                GdGMain.canvas.color(46,125,50),
                 GdGMain.canvas.color(0, 0, 0),
                 GdGMain.canvas.color(255, 255, 255));
         CColor LIGHT_GREEN = new CColor(GdGMain.canvas.color(104, 159, 56),
                 GdGMain.canvas.color(139, 195, 74),
-                GdGMain.canvas.color(139, 195, 74),
+                GdGMain.canvas.color(85,139,47),
                 GdGMain.canvas.color(0, 0, 0),
                 GdGMain.canvas.color(255, 255, 255));
         CColor LIME = new CColor(GdGMain.canvas.color(175, 180, 43),
                 GdGMain.canvas.color(205, 220, 57),
-                GdGMain.canvas.color(205, 220, 57),
+                GdGMain.canvas.color(158,157,36),
                 GdGMain.canvas.color(0, 0, 0),
                 GdGMain.canvas.color(255, 255, 255));
         CColor YELLOW = new CColor(GdGMain.canvas.color(251, 192, 45),
                 GdGMain.canvas.color(255, 235, 59),
-                GdGMain.canvas.color(255, 235, 59),
+                GdGMain.canvas.color(249,168,37),
                 GdGMain.canvas.color(0, 0, 0),
                 GdGMain.canvas.color(255, 255, 255));
         CColor AMBER = new CColor(GdGMain.canvas.color(255, 160, 0),
                 GdGMain.canvas.color(255, 193, 7),
-                GdGMain.canvas.color(255, 193, 7),
+                GdGMain.canvas.color(255,143,0),
                 GdGMain.canvas.color(0, 0, 0),
                 GdGMain.canvas.color(255, 255, 255));
         CColor ORANGE = new CColor(GdGMain.canvas.color(245, 124, 0),
                 GdGMain.canvas.color(255, 152, 0),
-                GdGMain.canvas.color(255, 152, 0),
+                GdGMain.canvas.color(239,108,0),
                 GdGMain.canvas.color(0, 0, 0),
                 GdGMain.canvas.color(255, 255, 255));
         CColor DEEP_ORANGE = new CColor(GdGMain.canvas.color(230, 74, 25),
                 GdGMain.canvas.color(255, 87, 34),
-                GdGMain.canvas.color(216, 67, 21),
+                GdGMain.canvas.color(216,67,21),
                 GdGMain.canvas.color(0, 0, 0),
                 GdGMain.canvas.color(255, 255, 255));
     }
