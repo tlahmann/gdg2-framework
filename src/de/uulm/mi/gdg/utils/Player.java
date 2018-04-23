457b76c18469459d35f01e9b0b5af69f2bb87f85
Tobias Lahmann <tobias.lahmann@uni-ulm.de>
2018-04-23 17:41:39 +0200

    Restructure and comment player class

--- src/de/uulm/mi/gdg/utils/Player.java
+++ src/de/uulm/mi/gdg/utils/Player.java
@@ -1,32 +1,33 @@
 package de.uulm.mi.gdg.utils;
 
 import ddf.minim.AudioPlayer;
-import ddf.minim.AudioSample;
 import ddf.minim.Minim;
-import ddf.minim.analysis.FFT;
 import de.uulm.mi.gdg.GdGMain;
-import processing.core.PApplet;
 
 /**
- * Created by Tobias Lahmann on 25.03.2017.
+ * Player class. The player stores and replays the song for the animation
+ *
+ * @author Tobias Lahmann
+ * Date: 25.03.2017.
  */
 public class Player {
     private AudioPlayer song;
 
-    public Player(String _file) {
+    /**
+     * Player constructor for the song to play in the animation
+     *
+     * @param filePath the path for the song
+     */
+    public Player(String filePath) {
         Minim minim = new Minim(GdGMain.canvas);
-        song = minim.loadFile(_file);
+        song = minim.loadFile(filePath);
     }
 
-    public void togglePlaying() {
-        if (!song.isPlaying()) {
-            song.play();
-        } else {
-            song.pause();
-            song.cue(0);
-        }
-    }
-
+    /**
+     * Getter method for the song
+     *
+     * @return the song set for this animation
+     */
     public AudioPlayer song() {
         return this.song;
     }
